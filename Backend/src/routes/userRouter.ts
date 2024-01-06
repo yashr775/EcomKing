0f4e3778e43fd9fc5a0ca5d80ba10523a6d31f7b
yashr775 <rajy29629@gmail.com>
2024-01-06 18:18:01 +0530

    userRouter completed

--- Backend/src/routes/userRouter.ts
+++ Backend/src/routes/userRouter.ts
@@ -5,7 +5,7 @@ import { PrismaClient } from '@prisma/client'
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 require('dotenv').config({ path: '../.env' });
-const JWT_SECRET = process.env.JWT_SECRET
+const JWT_SECRET = process.env.JWT_SECRET ||"some random secret"; 
 
 const prisma = new PrismaClient();
 
@@ -16,6 +16,11 @@ const createUserSchema = z.object({
     name:z.string().min(3,"Minimum 5 characters required"),
 email:z.string().email("Wrong email format"),
 password:z.string().min(5,"Minimum 5 characters required")});
+
+const loginUserSchema = z.object({
+    email:z.string().email("Wrong Email Format"),
+    password:z.string().min(5,"Minimum 5 characters required")
+});
 
 
 router.post('/createuser',async (req:Request,res:Response) => {
@@ -34,7 +39,7 @@ const salt = bcrypt.genSaltSync(saltRounds);
         const validationResult = createUserSchema.safeParse(userObj);
     
         if(!validationResult.success){
-            res.status(200).send(fromZodError(validationResult.error))
+            return res.status(200).send(fromZodError(validationResult.error))
         }
 
         const user = await prisma.user.findUnique({
@@ -42,7 +47,7 @@ const salt = bcrypt.genSaltSync(saltRounds);
         })
         
         if(user){
-            res.status(200).send("User with the given email already exist")
+           return  res.status(200).send("User with the given email already exist")
         }
 
         const createdUser = await prisma.user.create(
@@ -63,11 +68,57 @@ const salt = bcrypt.genSaltSync(saltRounds);
   } catch (error) {
         console.log('Internal server error');
         console.error('Internal server error :: ' + error);
-        res.status(500).send('Some error occurred');
+        return res.status(500).send('Some error occurred');
       }
       finally {
           await prisma.$disconnect();
         }
+})
+
+
+router.post("/login", async (req:Request,res:Response)=>{
+
+    try {
+        let success =false;
+
+        const {email , password} = req.body;
+
+        const  signinObj = {email,password};
+      
+        const  validationResult  = loginUserSchema.safeParse(signinObj);
+      
+        if(!validationResult.success){
+          res.status(400).send(fromZodError(validationResult.error))
+        }
+      
+        const user = await prisma.user.findUnique({where:{email}})
+      
+        if(!user){
+          return res.status(400).send("Invalid email or password");
+        }
+      
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+      
+        if(!isPasswordValid){
+          return res.status(400).send("Invalid username or password");
+        }
+
+        const data = {user : {_id : user.id}};
+
+        const token =jwt.sign(data,JWT_SECRET!);
+        success = true;
+
+        return res.status(200).json({success ,token})
+
+    } catch (error) {
+        console.log('Internal server error');
+        console.error('Internal server error :: ' + error);
+        return res.status(500).send('Some error occurred');
+    }finally{
+           prisma.$disconnect
+    }
+ 
+
 })
 
 module.exports =router
