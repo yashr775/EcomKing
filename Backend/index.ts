2b1ef1b8826ddc822b494dfb38891036f37405db
yashr775 <rajy29629@gmail.com>
2024-01-25 22:47:29 +0530

    order api created for payment integration

--- Backend/index.ts
+++ Backend/index.ts
@@ -12,6 +12,7 @@ const PORT = 5000;
 app.use('/api/user',require("./src/routes/userRouter"));
 app.use('/api/products',require("./src/routes/productRouter"));
 app.use('/api/orders',require("./src/routes/orderRouter"))
+app.use('/api/payment',require("./src/routes/paymentRouter"))
 
 app.listen(PORT,()=>{
     console.log(`App is listening on port ${PORT}`)
---
+++ Backend/src/routes/paymentRouter.ts
@@ -0,0 +1,43 @@
+import express,{Request,Response} from "express";
+import Razorpay from "razorpay";
+
+require('dotenv').config({path:'../.env'})
+
+const router=express.Router();
+
+const RAZORPAY_KEY_SECRET = process.env.RAZORPAY_KEY_SECRET;
+const RAZORPAY_KEY_ID = process.env.RAZORPAY_KEY_ID;
+
+router.post("/order",async (req:Request,res:Response)=>{
+
+    try {
+
+        if(!RAZORPAY_KEY_ID || !RAZORPAY_KEY_SECRET){
+            return res.status(500).send("Missing Key");
+        }
+
+        const razorpay =new Razorpay({
+            key_id:RAZORPAY_KEY_ID!,
+            key_secret:RAZORPAY_KEY_SECRET
+        })
+
+        const options =req.body;
+        console.log(options)
+        const order = await razorpay.orders.create(options);
+
+        if (!order) {
+            return res.status(500).send("Error");
+          }
+      console.log(order);
+         return res.json(order);
+
+        
+    } catch (error) {
+        console.error("Internal server error :: "+error);
+       return res.status(500).send("Some Error occured");
+    }
+
+})
+
+
+module.exports=router
